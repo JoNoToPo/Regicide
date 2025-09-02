40fae381b44ff07636294c6177f1c87ebae3e5ec
JoNoToPo <J.engelhard0@gmail.com>
2025-09-02 16:17:11 -0700

    Decompose suit parsing into helper function for attack

--- combat.py
+++ combat.py
@@ -20,13 +20,37 @@ def add_card(card, large_royal_played):
     return current_value, large_royal_played
 
 
+def block_attack(current_baddie, hand, card, discard_pile):
+    if len(hand) > 0:
+        block, _ = add_card(card, False)
+        current_baddie["this_attack"] -= block
+        discard_pile.append(card)
+        hand.remove(card)
+        return current_baddie
+    else:
+        return "Game Over"
+
+
+def suit_parse(card, suits, current_baddie):
+    if card[1] == input_color("♠", "BLACK", "WHITE") and current_baddie["Blocks"] != input_color("♠", "BLACK",
+                                                                                                 "WHITE"):
+        suits[3] = True
+    elif card[1] == input_color("♥", "RED", "WHITE") and current_baddie["Blocks"] != input_color("♥", "BLACK",
+                                                                                                 "WHITE"):
+        suits[1] = True
+    elif card[1] == input_color("♣", "BLACK", "WHITE") and current_baddie["Blocks"] != input_color("♣", "BLACK",
+                                                                                                   "WHITE"):
+        suits[0] = 2
+    elif card[1] == input_color("♦", "RED", "WHITE") and current_baddie["Blocks"] != input_color("♦", "RED",
+                                                                                                 "WHITE"):
+        suits[2] = True
+    return suits
+
+
 def attack(draw_pile, current_baddie, hand, hands, discard_pile, *user_input):
     attack_value = 0
     previous_value = 0
-    multiplier = 1
-    heals = False
-    draws = False
-    shield = False
+    suits = [1, False, False, False]
     large_royal_played = False
     values = [card[0] for card in user_input]
     if len(values) > 2:
@@ -41,47 +65,27 @@ def attack(draw_pile, current_baddie, hand, hands, discard_pile, *user_input):
                 (large_royal_played and not current_value <= 1 and not previous_value <= 1) or
                 (current_value != previous_value and not current_value <= 1 and not previous_value <= 1)):
             return input_color("Illegal pairing", "RED")
-        if card[1] == input_color("♠", "BLACK", "WHITE") and current_baddie["Blocks"] != input_color("♠", "BLACK",
-                                                                                                     "WHITE"):
-            shield = True
-        elif card[1] == input_color("♥", "RED", "WHITE") and current_baddie["Blocks"] != input_color("♥", "BLACK",
-                                                                                                     "WHITE"):
-            heals = True
-        elif card[1] == input_color("♣", "BLACK", "WHITE") and current_baddie["Blocks"] != input_color("♣", "BLACK",
-                                                                                                       "WHITE"):
-            multiplier = 2
-        elif card[1] == input_color("♦", "RED", "WHITE") and current_baddie["Blocks"] != input_color("♦", "RED",
-                                                                                                     "WHITE"):
-            draws = True
+        suits = suit_parse(card, suits, current_baddie)
         previous_value = int(current_value)
     for card in user_input:
         hand.remove(card)
-    if draws:
+    if suits[2]:
         if not hands:
             draw_cards(draw_pile, hand, attack_value)
         else:
             draw_cards(draw_pile, hand, attack_value, 9 - len(hands), hands)
-    if heals:
+    if suits[1]:
         for number in range(attack_value):
             if len(discard_pile) > 0:
                 drawn_card = random.choice(discard_pile)
                 discard_pile.remove(drawn_card)
                 draw_pile.insert(0, drawn_card)
-    if shield:
+    if suits[3]:
         current_baddie["this_attack"] -= attack_value
         current_baddie["damage"] -= attack_value
     for card in user_input:
         discard_pile.append(card)
-    current_baddie["health"] -= (attack_value * multiplier)
+    current_baddie["health"] -= (attack_value * suits[0])
     return current_baddie
 
 
-def block_attack(current_baddie, hand, card, discard_pile):
-    if len(hand) > 0:
-        block, _ = add_card(card, False)
-        current_baddie["this_attack"] -= block
-        discard_pile.append(card)
-        hand.remove(card)
-        return current_baddie
-    else:
-        return "Game Over"
